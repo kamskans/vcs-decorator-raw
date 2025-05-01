31e540812a7d844769ea0354b73c8dff7a9e478c
kamskans <127218952+kamskans@users.noreply.github.com>
2025-05-02 00:10:06 +0100

    Add raw video grid decorator component

---
+++ decorateVideoGridItem.js
@@ -0,0 +1,44 @@
+/*
+  This is an override point for video grid item decoration.
+  The function gets called for each item in the grid.
+
+  The default rendering for labels and highlight can also be toggled off here.
+
+  Input arguments:
+  * `itemIndex`: the index of the item being decorated.
+  * `itemProps`: props passed to create this item (see VideoGrid.js).
+  * `gridProps`: props passed to the grid itself (see VideoGrid.js).
+
+  Return object props:
+  * `enableDefaultLabels`: render the default participant labels.
+  * `enableDefaultHighlight`: render the default highlighting.
+  * `customComponent`: a custom VCS component to be rendered.
+  * `clipItem`: if true, the custom component graphics are clipped inside
+                the video item's frame.
+  * `customLayoutForVideo`: a layout applied to the item's video element.
+  
+  If you return a custom component, it gets rendered last,
+  on top of the default labels + highlight.
+*/
+
+import * as React from 'react';
+import { Box } from '#vcs-react/components';
+
+export default function decorateVideoGridItem(itemIndex, itemProps, gridProps) {
+  return {
+    enableDefaultLabels: true,
+    enableDefaultHighlight: false,
+    customComponent: (
+      <Box
+        style={{
+          strokeColor: '#FFFFFF',
+          strokeWidth_px: 12,
+          cornerRadius_px: 12,
+          fill: 'none'
+        }}
+      />
+    ),
+    clipItem: true,
+    customLayoutForVideo: null
+  };
+}
