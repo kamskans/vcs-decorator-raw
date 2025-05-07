a552a78b0661b50af5db909c271d3a7f8be0427c
kamskans <127218952+kamskans@users.noreply.github.com>
2025-05-07 20:31:40 +0100

    Create customtiledominant.js

---
+++ customtiledominant.js
@@ -0,0 +1,46 @@
+/*
+  This is an override point.
+
+  You can replace this function to return custom decorator components
+  for video dominant items. The function gets called for each item.
+
+  The default rendering for labels and highlight can also be toggled off here.
+
+  Input arguments:
+  * `itemIndex`: the index of the item being decorated.
+  * `itemProps`: props passed to create this item (see VideoDominant.js).
+  * `dominantProps`: props passed to the dominant itself (see VideoDominant.js).
+
+  Return object props:
+  * `enableDefaultLabels`: render the default participant labels.
+  * `enableDefaultHighlight`: render the default highlighting.
+  * `customComponent`: a custom VCS component to be rendered.
+  * `clipItem`: if true, the custom component graphics are clipped inside
+                the video item's frame.
+  * `customLayoutForVideo`: a layout applied to the item's video element.
+  
+  If you return a custom component, it gets rendered last,
+  on top of the default labels + highlight.
+*/
+
+import * as React from 'react';
+import { Box } from '#vcs-react/components';
+
+export default function decorateVideoDominantItem(itemIndex, itemProps, dominantProps) {
+  return {
+    enableDefaultLabels: false,
+    enableDefaultHighlight: true,
+    customComponent: (
+      <Box
+        style={{
+          strokeColor: '#FFFFFF',
+          strokeWidth_px: 12,
+          cornerRadius_px: 12,
+          pointerEvents: 'none',
+        }}
+      />
+    ),
+    clipItem: true,
+    customLayoutForVideo: null
+  };
+}
